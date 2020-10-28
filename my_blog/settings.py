3dae252283902a340997d749adc7c7f59efaf3f8
ggranda <giovanotti1906@hotmail.com>
2020-10-27 21:03:13 -0500

    Added CK editor for entries of blog

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
     'main_app',
     'pages_app.apps.PagesAppConfig',
 ]
--- pages_app/models.py
+++ pages_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
 
 class Page(models.Model):
     title = models.CharField(max_length=50, verbose_name='Título')
-    content = models.TextField(verbose_name='Contenido')
+    content = RichTextField(verbose_name='Contenido')
     slug = models.CharField(max_length=150, verbose_name='URL Amigable')
     visible = models.BooleanField(verbose_name='Es visible')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha Creación')
